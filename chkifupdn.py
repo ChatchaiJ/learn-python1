07caccfeb48751a4952c347690691a79f3a8e301
Chatchai J <chatchai.j@gmail.com>
2025-09-06 18:16:31 +0700

    check interface up/down -- chkifupdn.py

---
+++ chkifupdn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+import socket
+import struct
+import fcntl
+import nmap
+
+SAMPLE_PORTS = '21-23'
+
+def get_interface_status(ifname):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	ip_address = socket.inet_ntoa(fcntl.ioctl(
+		sock.fileno(),
+		0x8915, # SIOCGIFADDR, C socket library sockios.h
+		struct.pack('256s', bytearray(ifname[:15], 'ascii'))
+	)[20:24])
+
+	nm = nmap.PortScanner()
+	nm.scan(ip_address, SAMPLE_PORTS)
+	return nm[ip_address].state()
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="Python networking utils")
+	parser.add_argument('--ifname', action="store", dest="ifname", required=True)
+	given_args = parser.parse_args()
+	ifname = given_args.ifname
+	print(f"Interface [{ifname}] is: {get_interface_status(ifname)}")
